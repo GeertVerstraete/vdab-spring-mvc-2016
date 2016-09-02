f3a3f2f9b355659730d6f78772b51bf5ec8f46ec
Geert Verstraete <geert.verstraete@gmail.com>
2016-09-02 13:05:30 +0200

    ready for pull

---
+++ src/main/java/hello/Main.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * Created by aryastark on 02/09/16.
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
